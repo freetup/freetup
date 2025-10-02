5e21a04d9fa0506f93c4943bf89bd5b80d27c691
Jack Allen <jackallensemail+git@gmail.com>
2025-10-02 15:40:53 +0100

    stacge

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -3,7 +3,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 1000,
+    },
+  },
+});
 
 export function RootLayoutClient({ children }: PropsWithChildren) {
   return (
